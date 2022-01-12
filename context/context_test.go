2a6bd5bb06b721beb56e9747a382624f8680377f
zhaofuqing <zhaofuqing@chemball.com>
2022-01-12 18:02:12 +0800

    无逻辑变化

--- context/context_test.go
+++ context/context_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"path"
+	"regexp"
 	"strings"
 	"testing"
 
@@ -70,4 +71,22 @@ func performRequest(r http.Handler, method, path string) *httptest.ResponseRecor
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	return w
+}
+
+func TestRegexp(t *testing.T) {
+	src := "ATS-8; Tert-Butyl (4R,6R)-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate; (4R-Cis)-1,1-Dimethylethyl-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate; (4R,6R)-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetic Acid Tert-Butyl Ester; Tert-Butyl 6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxolane-4-Acetate; (4R,6R)-T-Butyl-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate; (4R,Cis)-1,1-Dimethylethyl-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate; (4R,3R)-Tert-Butyl-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate; (4R-Cis)-1,1-Dimethylethyl-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate (Ats-8); Tert-Butyl[(4R,6R)-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxan-4-Yl]Acetate; Atorvastatin Calcium Intermediate ats-8; ATS-8: (4R-cis)-1,1-Dimethylethyl-6-Cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate"
+
+	//keyword := `(?i)\b`+function.AddEscapedChar("(4R,6R)-6-cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate")+`\b`
+	keyword := `(?i)\(4R,6R\)-6-cyanomethyl-2,2-Dimethyl-1,3-Dioxane-4-Acetate`
+	fmt.Println(keyword)
+	regExp := regexp.MustCompile(keyword)
+
+	repl := `<span style="color: red;">$0</span>`
+
+	result := regExp.ReplaceAllString(src, repl)
+	fmt.Println(result)
+
+	// 特殊字符的查找
+	//reg = regexp.MustCompile(`[\f\t\n\r\v\123\x7F\x{10FFFF}\\\^\$\.\*\+\?\{\}\(\)\[\]\|]`)
+	//fmt.Printf("%q\n", reg.ReplaceAllString("\f\t\n\r\v\123\x7F\U0010FFFF\\^$.*+?{}()[]|", "-"))
 }
